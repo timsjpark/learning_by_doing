7bf00786be692c2eaa2ac273bb00af4aadc65f59
Tim Park <timsjpark@gmail.com>
2015-10-04 18:53:24 -0600

    Modify number_of_seconds.rb
    Fixed code to replace numbers in lines 12-15 with interpolated
    variables. Also fixed last four lines of code to combine a print and
    puts command to print a string on one line instead of using a '+' or
    a '\' to concatenate multiple strings.
    
    [#104778992]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -9,15 +9,15 @@ seconds_per_hour = seconds_per_minute * minutes_per_hour
 seconds_per_day = seconds_per_hour * hours_per_day
 seconds_per_year = seconds_per_day * days_per_year
 
-puts 'There are 60 seconds in a minute'
-puts 'There are 60 minutes in an hour'
-puts 'There are 24 hours in a day'
-puts 'There are 7 days in a week'
+puts "There are #{seconds_per_minute} seconds in a minute"
+puts "There are #{minutes_per_hour} minutes in an hour"
+puts "There are #{hours_per_day} hours in a day"
+puts "There are #{days_per_week} days in a week"
 puts 'That means there are:'
 puts "  #{seconds_per_hour} seconds in an hour,"
 puts "  #{seconds_per_day} seconds in a day,"
 puts "  #{seconds_per_day * days_per_week} seconds in a week"
-puts "That means when you turn 20, you've been alive for " +
-     "#{20 * seconds_per_year} seconds,"
-puts "and if you make it to 100, you will have lived " +
-     "#{100 * seconds_per_year} seconds. Make them count!"
+print "That means when you turn 20, you've been alive for "
+puts "#{20 * seconds_per_year} seconds,"
+print "and if you make it to 100, you will have lived "
+puts "#{100 * seconds_per_year} seconds. Make them count!"
