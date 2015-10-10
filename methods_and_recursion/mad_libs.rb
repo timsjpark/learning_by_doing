e4c28e8dfae35aaaeae939d05cbbfccba3e1108e
Tim Park <timsjpark@gmail.com>
2015-10-10 14:00:34 -0600

    Modify mad_libs.rb to rename variable assignments
    Fix variables in lines 11 - 23 to follow best Ruby style practices.
    Specifically, change variables ending in a number to add an
    underscore that separates the number from the preceding word.
    
    [#105164714]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -9,14 +9,14 @@ end
 # in order to avoid receiving warnings from Rubocop.
 
 adj = 'an adjective'
-adj2 = 'another adjective'
+adj_2 = 'another adjective'
 noun = 'a noun'
-noun2 = 'another noun'
+noun_2 = 'another noun'
 plu_noun = 'a plural noun'
-plu_noun2 = 'another plural noun'
+plu_noun_2 = 'another plural noun'
 game = 'a game'
 verb_ing = "a verb ending in 'ing'"
-verb_ing2 = "another verb ending in 'ing'"
+verb_ing_2 = "another verb ending in 'ing'"
 plant = 'a plant'
 body = 'a part of the body'
 place = 'a place'
@@ -25,19 +25,19 @@ number = 'a number'
 puts <<-EOT.sub(/\n$/, '')
 Here is your madlib:
 A vacation is when you take a trip to some #{get_word(adj)} place
-with your #{get_word(adj2)} family. Usually you go to some place
-that is near a/an #{get_word(noun)} or up on a/an #{get_word(noun2)}.
+with your #{get_word(adj_2)} family. Usually you go to some place
+that is near a/an #{get_word(noun)} or up on a/an #{get_word(noun_2)}.
 A good vacation place is one where you can ride #{get_word(plu_noun)}
-or play #{get_word(game)} or go hunting for #{get_word(plu_noun2)}. I like
-to spend my time #{get_word(verb_ing)} or #{get_word(verb_ing2)}.
+or play #{get_word(game)} or go hunting for #{get_word(plu_noun_2)}. I like
+to spend my time #{get_word(verb_ing)} or #{get_word(verb_ing_2)}.
 When parents go on a vacation, they spend their time eating
-three #{get_word(plu_noun2)} a day, and fathers play golf, and mothers
-sit around #{get_word(verb_ing2)}. Last summer, my little brother
-fell in a/an #{get_word(noun2)} and got poison #{get_word(plant)} all
+three #{get_word(plu_noun_2)} a day, and fathers play golf, and mothers
+sit around #{get_word(verb_ing_2)}. Last summer, my little brother
+fell in a/an #{get_word(noun_2)} and got poison #{get_word(plant)} all
 over his #{get_word(body)}. My family is going to go to (the)
-#{get_word(place)}, and I will practice #{get_word(verb_ing2)}. Parents
+#{get_word(place)}, and I will practice #{get_word(verb_ing_2)}. Parents
 need vacations more than kids because parents are always very
-#{get_word(adj2)} and because they have to work #{get_word(number)}
-hours every day all year making enough #{get_word(plu_noun2)} to pay
+#{get_word(adj_2)} and because they have to work #{get_word(number)}
+hours every day all year making enough #{get_word(plu_noun_2)} to pay
 for the vacation.
 EOT
