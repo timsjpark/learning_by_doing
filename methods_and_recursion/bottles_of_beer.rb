c9688391eb6ca5b876d8c81ad48d14f017e3af5b
Tim Park <timsjpark@gmail.com>
2015-10-11 17:27:06 -0600

    Modify bottles_of_beer.rb
    Fix code to combine pluralize_first and pluralize_second methods.
    Mostly started from scratch
    
    [#105164746]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,9 +1,5 @@
 #!/usr/bin/env ruby
 
-# WARNING! - LineLength cop disabled for this program to account
-# WARNING! - for long strings in line 32.
-# rubocop:disable Metrics/LineLength
-
 # Create method to automatically add spacing between lines
 
 def spacing(first, second, option = true)
@@ -13,52 +9,68 @@ def spacing(first, second, option = true)
   puts '' if option == true
 end
 
-# Method to print out first part of each verse. Prints out 'bottle'
-# when the number of bottles is 1. If number argument is a string,
-# like 'no more' at the end of the song, pluralize_first will
-# capitalize the first iteration of the string.
+# This method prints out the normal second_verse. It accounts for the
+# special verse when number == 1  and print 'no more bottles' instead
+# of '0 bottles'. At the very end of the song when number == 'no more',
+# it prints a special end verse.
 
-def pluralize_first(number, bottle = 'bottles')
-  bottle = 'bottle' if number == 1
-  written_number = number
-  "#{written_number.to_s.capitalize} #{bottle} of beer on the wall, " \
-  "#{written_number} #{bottle} of beer."
-end
-
-# Method to print out second part of verse. Prints out 'bottle' if
-# the verse refers to 1 bottle left (number == 2) and 'no more bottles'
-# when there are no bottles left (number == 1). Also, it prints out a
-# special line at the very end of the song.
-
-def pluralize_second(number, bottle = 'bottles', remaining = number - 1)
-  bottle = 'bottle' if number == 2
-  remaining = 'no more' if number == 1
-  if number == 0
+# Disable Rubocop for this method to account for long string on line 23
+# rubocop:disable Metrics/LineLength
+def second_verse(number, bottle_2)
+  if number == 'no more'
     'Go to the store and buy some more, 99 bottles of beer on the wall.'
   else
-    "Take one down and pass it around, #{remaining} #{bottle} of beer on the wall."
+    if number > 1
+      remaining = number - 1
+    else
+      remaining = 'no more' if number == 1
+    end
+    "Take one down and pass it around, #{remaining} #{bottle_2} of beer on the wall."
+  end
+end
+# rubocop:enable Metrics/LineLength
+
+# This method passes true to the option argument in the spacing method
+# except for at the end of the song (number == 'no more')
+
+def option(number)
+  if number == 'no more'
+    false
+  else
+    true
   end
 end
 
-# Method for normal song output
+# The print_lyrics method outputs one set of verses from the song,
+# '99 bottles of beer'. Given a number, it will print out the song
+# lyrics and account for writing 'bottle' in singular form when
+# the song refers to just 1 bottle.
+#
+# It also accounts for the case  of 0 bottles, and prints out 'no more'
+# instead of the number. Pay attention that number is re-assigned the
+# value 'no more' when the 'number' argument == 0. As such, the
+# second_verse and option methods evaluate if number == 'no more' and
+# not if number == 0
 
-def normal_song_verse(number)
-  spacing(pluralize_first(number),
-          pluralize_second(number))
+def print_lyrics(number, bottle_1 = bottle_2 = 'bottles')
+  bottle_2 = 'bottle' if number == 2
+  bottle_1 = 'bottle' if number == 1
+  number = 'no more' if number == 0 # number variable is re-assigned
+  first = "#{number.to_s.capitalize} #{bottle_1} of beer on the wall, " \
+          "#{number} #{bottle_1} of beer."
+  second = second_verse(number, bottle_2)
+  option = option(number)
+  spacing(first, second, option)
 end
 
-# Print out song output normally and recursively execute method until
-# number == 0. Then print out special end of song output and EXIT.
+# Print out song lyrics recursively and exit when number == 0
 
 def beer_song(number = 99)
-  if number == 0
-    spacing(pluralize_first('no more'),
-            pluralize_second(number),
-            false)
-    exit
-  end
-  normal_song_verse(number)
+  print_lyrics(number)
+  exit if number == 0
   beer_song(number - 1)
 end
+
+# Print the song, computer! Dance, monkey!
 
 puts beer_song
