3bbcc24d3574da9ae9af033589c70cd7605dd529
Tim Park <timsjpark@gmail.com>
2015-10-10 19:54:15 -0600

    Re-write fizz_buzz_array.rb to use built in Array methods
    Create a small method that uses the built in Array.map! method
    to modify an existing array to contain the strings Fizz, Buzz,
    and FizzBuzz where relevant.
    
    [#105165670]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -2,26 +2,38 @@
 
 numbers = (1..100).to_a
 
-def replace_fizz_buzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    'FizzBuzz'
-  elsif number % 3 == 0
-    'Fizz'
-  elsif number % 5 == 0
-    'Buzz'
-  else
-    number
-  end
-end
-
-def fizz_buzz(array, index = 0)
-  array[index] = replace_fizz_buzz(array[index])
-  if index == array.length - 1
-    return(array)
-  else
-    next_index = index + 1
-  end
-  fizz_buzz(array, next_index)
+def fizz_buzz(array)
+  array.map! { |i| i % 3 == 0 && i % 5 == 0 ? 'FizzBuzz' : i }
+  array.map! { |i| i % 5 == 0 ? 'Buzz' : i }
+  array.map! { |i| i % 3 == 0 ? 'Fizz' : i }
+  array
 end
 
 puts fizz_buzz(numbers)
+
+# Previous work commented out to practice modifying an existing
+# array using Array specific methods.
+
+# def replace_fizz_buzz(number)
+#   if number % 3 == 0 && number % 5 == 0
+#     'FizzBuzz'
+#   elsif number % 3 == 0
+#     'Fizz'
+#   elsif number % 5 == 0
+#     'Buzz'
+#   else
+#     number
+#   end
+# end
+#
+# def fizz_buzz(array, index = 0)
+#   array[index] = replace_fizz_buzz(array[index])
+#   if index == array.length - 1
+#     return(array)
+#   else
+#     next_index = index + 1
+#   end
+#   fizz_buzz(array, next_index)
+# end
+#
+# puts fizz_buzz(numbers)
