2c373796176ed0c322a4de94548c05cf6f52ad6c
Tim Park <timsjpark@gmail.com>
2015-10-11 18:38:52 -0600

    Modify fizz_buzz_array.rb to create new fizz_buzz_numbers array.
    Add replace_fizz_buzz method that converts numbers to appropriate
    Fizz_Buzz string. Used Array.map method and passed in the
    replace_fizz_buzz method to create a new array called
    fizz_buzz_numbers.
    
    [#105165670]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
 
-numbers = (1..100).to_a
-
-def fizz_buzz(array)
-  array.map! { |i| i % 3 == 0 && i % 5 == 0 ? 'FizzBuzz' : i }
-  array.map! { |i| i % 5 == 0 ? 'Buzz' : i }
-  array.map! { |i| i % 3 == 0 ? 'Fizz' : i }
-  array
+def replace_fizz_buzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number
+  end
 end
 
-puts fizz_buzz(numbers)
+numbers = (1..100).to_a
+fizz_buzz_numbers = numbers.map { |i| replace_fizz_buzz(i) }
+puts fizz_buzz_numbers
 
 # Previous work commented out to practice modifying an existing
 # array using Array specific methods.
