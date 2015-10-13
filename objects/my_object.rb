2c47f94e8b4becd0b1122b53b037a5cf327fce5c
Tim Park <timsjpark@gmail.com>
2015-10-12 23:40:39 -0600

    Modify my_object.rb to nearly complete Oven class.
    Fix code to have the Oven class nearly working as I want. Rename
    Oven class to SassyOven. Many offenses detected by guard-rubocop.
    
    [#105361970]

--- objects/my_object.rb
+++ objects/my_object.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # BAKE ALL THE THINGS!!!
-class Oven
+class SassyOven
   def initialize
     @temp = 80
     @filth = 0
@@ -15,20 +15,10 @@ class Oven
     puts "Temperature: #{@temp} °F"
   end
 
-  # Takes timer settings and reduces it to 1/10th of the original
-  # time because this is a fake Oven and I don't want you to wait
-  # 60 real minutes for a prompt. Ain't nobody got time for that!
-
-  def format_time(time)
-    if (time % 10) >= 5
-      round = 1
-    else
-      round = 0
-    end
-    (time / 10) + round
-  end
+  # Who doesn't like a self-cleaning oven?
 
   def self_clean
+    return 'Maybe you should close the door.' if @door == 'open'
     heat_up(700, :fast)
     puts "Your oven is now self_cleaning. Do not open the door."
     5.times do
@@ -48,16 +38,16 @@ class Oven
       puts 'You open the door.'
       puts "It's hot in there!" if @temp > 250
       if Time.new >= (@burnt + 10)
-        puts 'Whoa! You burned your food!' \
+        puts 'Whoa! You burned your food! ' \
              'Maybe you should take it out and clean the oven.'
       elsif @filth >= 10 && Time.new < (@burnt + 10)
         puts 'Eww...your oven is gross. Maybe you should clean it.'
       end
       @door = 'open'
     else
-      puts 'The door is already open.'
-      puts 'Whoa! You burned your food! ' +
-          'Maybe you should take it out and clean the oven.' if Time.new >= (@burnt + 10)
+      puts 'Are you blind? The door is already open.'
+      puts 'Whoa! You burned your food! ' \
+           'Maybe you should take it out and clean the oven.' if Time.new >= (@burnt + 10)
       puts 'Eww...your oven is gross. Maybe you should clean it.' if (@filth >= 10 && Time.new < (@burnt + 10))
     end
   end
@@ -67,15 +57,68 @@ class Oven
   def close
     if @door == 'open'
       puts 'You close the door.'
+      if Time.new >= (@burnt + 10) || @filth >= 10
+        puts "You're just going to ignore the mess? I hope your wife doesn't see it..."
+      end
       @door = 'close'
     else
-      puts 'The door is already closed.'
+      puts 'Are you blind? The door is already closed.'
+    end
+  end
+
+  # Takes timer settings and reduces it to 1/10th of the original
+  # time because this is a fake Oven and I don't want you to wait
+  # 60 real minutes for a prompt. Ain't nobody got time for that!
+
+  def format_time(time)
+    if (time % 10) >= 5
+      round = 1
+    else
+      round = 0
+    end
+    (time / 10) + round
+  end
+
+  # The broil method allows you to cook food at 500 F automatically.
+
+  def broil
+    heat_up(500, :fast)
+    bake
+  end
+
+  def closed_door_test
+    index = 1
+    while @door == 'open'
+      puts 'Baking will not begin until you close the door.' if index >= 3
+      puts 'Close the door by typing close --> '
+      test = gets.chomp
+      @door = 'close' if test == 'close'
+      index += 1
+    end
+  end
+
+  # This should use bake_method but make sure that the door is
+  # open or closed first.
+
+  def bake
+    if @door == 'open'
+      puts "Please put your food in the oven and close the door by typing 'close' --> "
+      test = gets.chomp
+      if test == 'close'
+        @door = 'close'
+        bake_method
+      else
+        closed_door_test
+        bake_method
+      end
+    else
+      return 'Please open the oven door first.'
     end
   end
 
   # Bake some food
 
-  def bake
+  def bake_method
     if Time.new >= (@burnt + 10) || @filth >= 10
       puts 'Maybe you should clean the oven first...'
     end
@@ -99,13 +142,13 @@ class Oven
 
   def select_temp(temp)
     if @temp < 80
-      puts 'Your oven cannot go lower than room temperature.'
-      puts 'Please try and set a new temperature.'
+      return 'Your oven cannot go lower than room temperature. ' \
+           'Please try and set a new temperature.'
     elsif @temp < temp
       heat_up(temp)
     elsif @temp > temp
-      puts 'Your oven is still hot.'
-      puts 'Please wait for it to cool down before baking again.'
+      puts 'Your oven is still hot. ' \
+           'Please wait for it to cool down before baking again.'
       cool_down(temp)
       puts "Your oven is now holding at #{@temp} °F."
     else
@@ -113,10 +156,10 @@ class Oven
     end
   end
 
-  # Turns the oven off and cools to 80 F
+  # Turns the oven off and cools to 80 F (room temperature)
 
   def off
-    cool_down(80)
+    cool_down(80, :fast)
     puts 'Your oven is fully cooled down.'
   end
 
@@ -143,5 +186,9 @@ class Oven
       @temp = temp if @temp < temp
       puts "Temperature: #{@temp} °F"
     end
+  end
+
+  def test
+    test_method
   end
 end
