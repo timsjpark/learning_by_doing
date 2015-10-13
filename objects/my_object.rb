23317f5b5e6908c7e564e81e0bee9eded7562dcb
Tim Park <timsjpark@gmail.com>
2015-10-13 16:44:43 -0600

    Modify my_object.rb to fix rubocop erros and clean code.
    SassyOven is now devoid of offenses detected by rubocop and has
    code that is easy to read and interpret. I made a new file called
    my_object_auxiliary_methods.rb to contain methods that are
    essential to the function of SassyOven, but that the user doesn't
    need to call. So I hid them from the user all together. I may have
    silenced Rubocop in some sections, but only because it thought
    that my methods/lines were too long and I knew better.
    
    [#105361970]

--- objects/my_object.rb
+++ objects/my_object.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 
+require_relative 'my_object_auxiliary_methods'
+
 # BAKE ALL THE THINGS!!!
 class SassyOven
   def initialize
     @temp = 80
     @filth = 0
     @door = 'close'
-    @burnt = Time.new + 60*60 # Just so things aren't already burnt
+    @burnt = Time.new + 60 * 60 # Just so things aren't already burnt
   end
 
   # Print the current temperature
@@ -18,17 +20,14 @@ class SassyOven
   # Who doesn't like a self-cleaning oven?
 
   def self_clean
-    return 'Maybe you should close the door.' if @door == 'open'
+    return 'Maybe you should close the door first.' if @door == 'open'
     heat_up(700, :fast)
-    puts "Your oven is now self_cleaning. Do not open the door."
-    5.times do
-      puts '...'
-      sleep(1)
-    end
+    puts 'Your oven is now self_cleaning. Do not open the door.'
+    self_clean_step
     puts 'Your oven is now clean! Please wait for your oven to cool down.'
-    cool_down(80, :fast)
+    off
     @filth = 0
-    @burnt = Time.new + 60*60
+    @burnt = Time.new + 60 * 60
   end
 
   # Opens the door
@@ -36,19 +35,11 @@ class SassyOven
   def open
     if @door == 'close'
       puts 'You open the door.'
-      puts "It's hot in there!" if @temp > 250
-      if Time.new >= (@burnt + 10)
-        puts 'Whoa! You burned your food! ' \
-             'Maybe you should take it out and clean the oven.'
-      elsif @filth >= 10 && Time.new < (@burnt + 10)
-        puts 'Eww...your oven is gross. Maybe you should clean it.'
-      end
+      if_the_oven_is_gross
       @door = 'open'
     else
       puts 'Are you blind? The door is already open.'
-      puts 'Whoa! You burned your food! ' \
-           'Maybe you should take it out and clean the oven.' if Time.new >= (@burnt + 10)
-      puts 'Eww...your oven is gross. Maybe you should clean it.' if (@filth >= 10 && Time.new < (@burnt + 10))
+      if_the_oven_is_gross
     end
   end
 
@@ -58,7 +49,8 @@ class SassyOven
     if @door == 'open'
       puts 'You close the door.'
       if Time.new >= (@burnt + 10) || @filth >= 10
-        puts "You're just going to ignore the mess? I hope your wife doesn't see it..."
+        puts "You're just going to ignore the mess? " \
+             "I hope your wife doesn't see it..."
       end
       @door = 'close'
     else
@@ -66,18 +58,26 @@ class SassyOven
     end
   end
 
-  # Takes timer settings and reduces it to 1/10th of the original
-  # time because this is a fake Oven and I don't want you to wait
-  # 60 real minutes for a prompt. Ain't nobody got time for that!
+  # Set a temperature that you want for baking. The oven will
+  # heat up or cool down to reach that temperature.
 
-  def format_time(time)
-    if (time % 10) >= 5
-      round = 1
-    else
-      round = 0
+  # Disable MethodLength cop because select_temp method is only 1 line
+  # over the limit and I don't want to break the LineLength rule here.
+  # rubocop:disable Metrics/MethodLength
+  def preheat(temp)
+    puts "Your oven is already holding at #{@temp} °F." if @temp == temp
+    if temp < 80
+      return 'Your oven is not a fridge and cannot go lower than room ' \
+             'temperature. Please try and set a new temperature.'
+    elsif @temp < temp
+      heat_up(temp)
+    elsif @temp > temp
+      puts 'Your oven is still hot. ' \
+           'Please wait for it to cool down before baking again.'
+      cool_down(temp)
     end
-    (time / 10) + round
   end
+  # rubocop:enable Metrics/MethodLength
 
   # The broil method allows you to cook food at 500 F automatically.
 
@@ -86,73 +86,22 @@ class SassyOven
     bake
   end
 
-  def closed_door_test
-    index = 1
-    while @door == 'open'
-      puts 'Baking will not begin until you close the door.' if index >= 3
-      puts 'Close the door by typing close --> '
-      test = gets.chomp
-      @door = 'close' if test == 'close'
-      index += 1
-    end
-  end
-
-  # This should use bake_method but make sure that the door is
-  # open or closed first.
+  # This allows you to bake food. This method should make sure that the door
+  # is open first and then use the bake_process method to cook the food.
 
   def bake
+    # Leave me alone Rubocop. I know what I'm doing.
+    # rubocop:disable Metrics/LineLength
+    return "Please pre-heat the oven. Nothing's going to cook at #{@temp}" if @temp < 200
+    # rubocop:enable Metrics/LineLength
     if @door == 'open'
-      puts "Please put your food in the oven and close the door by typing 'close' --> "
-      test = gets.chomp
-      if test == 'close'
-        @door = 'close'
-        bake_method
-      else
-        closed_door_test
-        bake_method
+      if Time.new >= (@burnt + 10) || @filth >= 10
+        return 'Maybe you should clean the oven first...'
       end
+      closed_door_test
+      bake_process
     else
       return 'Please open the oven door first.'
-    end
-  end
-
-  # Bake some food
-
-  def bake_method
-    if Time.new >= (@burnt + 10) || @filth >= 10
-      puts 'Maybe you should clean the oven first...'
-    end
-    print "Would you like to set a timer? Reply 'yes' or 'no' --> "
-    reply = gets.chomp
-    if reply == 'yes'
-      print 'Please set the timer in minutes --> '
-      time_set = gets.chomp.to_i
-      @timer = format_time(time_set)
-      puts "The alarm will sound in #{time_set} minutes."
-      sleep(@timer)
-      puts "\aDING! Your food is ready! Don't forget to turn off the oven."
-    else
-      @burnt = Time.new
-    end
-    @filth += rand(1..4)
-  end
-
-  # Set a temperature that you want for baking. The oven will
-  # heat up or cool down to reach that temperature.
-
-  def select_temp(temp)
-    if @temp < 80
-      return 'Your oven cannot go lower than room temperature. ' \
-           'Please try and set a new temperature.'
-    elsif @temp < temp
-      heat_up(temp)
-    elsif @temp > temp
-      puts 'Your oven is still hot. ' \
-           'Please wait for it to cool down before baking again.'
-      cool_down(temp)
-      puts "Your oven is now holding at #{@temp} °F."
-    else
-      puts "Your oven is already holding at #{@temp} °F."
     end
   end
 
@@ -161,34 +110,5 @@ class SassyOven
   def off
     cool_down(80, :fast)
     puts 'Your oven is fully cooled down.'
-  end
-
-  # Heats up the oven
-
-  def heat_up(temp, speed = :regular)
-    how_fast = {regular: 50, fast: 100}
-    while @temp < temp
-      @temp += how_fast[speed]
-      sleep(1)
-      @temp = temp if @temp > temp
-      puts "Temperature: #{@temp} °F"
-    end
-    puts "Your oven is now holding at #{@temp} °F."
-  end
-
-  # Cools down the oven
-
-  def cool_down(temp, speed = :regular)
-    how_fast = {regular: 50, fast: 100}
-    while @temp > temp
-      @temp -= how_fast[speed]
-      sleep(1)
-      @temp = temp if @temp < temp
-      puts "Temperature: #{@temp} °F"
-    end
-  end
-
-  def test
-    test_method
   end
 end
