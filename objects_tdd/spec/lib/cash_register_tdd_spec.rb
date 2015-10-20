a9f9d3a5bcd8af4ba264010503b7eeee456e90d3
Tim Park <timsjpark@gmail.com>
2015-10-19 20:40:29 -0600

    Modify cash_register_tdd_spec.rb to use better testing conventions
    Add a 'subject' at the beginning of the test so each separate test
    does not have to create a new CashRegister object.
    
    [#105904512]

--- objects_tdd/spec/lib/cash_register_tdd_spec.rb
+++ objects_tdd/spec/lib/cash_register_tdd_spec.rb
@@ -1,54 +1,49 @@
 require './lib/cash_register_tdd'
 
 describe CashRegister do
+  subject { CashRegister.new }
+
   describe '#total' do
     it 'should print a starting total of $0.00' do
-      register = CashRegister.new
-      expect(register.total).to eq('$0.00')
+      expect(subject.total).to eq('$0.00')
     end
 
     it 'should print the correct total after a payment or purchase' do
-      register = CashRegister.new
-      register.purchase(4.12)
-      expect(register.total).to eq('$4.12')
-      register.pay(3)
-      expect(register.total).to eq('$1.12')
+      subject.purchase(4.12)
+      expect(subject.total).to eq('$4.12')
+      subject.pay(3)
+      expect(subject.total).to eq('$1.12')
     end
   end
 
   describe '#purchase' do
     it 'should add a specified amount to the total' do
-      register = CashRegister.new
-      expect(register.purchase(4.12)).to eq(4.12)
+      expect(subject.purchase(4.12)).to eq(4.12)
     end
 
     it 'should modify the total to reflect the purchase' do
-      register = CashRegister.new
-      register.purchase(4.12)
-      expect(register.total).to eq('$4.12')
+      subject.purchase(4.12)
+      expect(subject.total).to eq('$4.12')
     end
   end
 
   describe '#pay' do
     context 'should subtract a specified amount from the total and' do
       it 'should return a string if there is payment left to be made' do
-        register = CashRegister.new
-        register.purchase(2.18)
-        expect(register.pay(1.17)).to eq('You still owe $1.01')
+        subject.purchase(2.18)
+        expect(subject.pay(1.17)).to eq('You still owe $1.01')
       end
 
       it 'should return a string if the total has been fully paid' do
-        register = CashRegister.new
-        register.purchase(2.18)
-        expect(register.pay(3)).to eq('Your change is $0.82')
+        subject.purchase(2.18)
+        expect(subject.pay(3)).to eq('Your change is $0.82')
       end
     end
 
     it 'should modify the total to reflect the payment' do
-      register = CashRegister.new
-      register.purchase(1.23)
-      register.pay(1.23)
-      expect(register.total).to eq('$0.00')
+      subject.purchase(1.23)
+      subject.pay(1.23)
+      expect(subject.total).to eq('$0.00')
     end
   end
 end
