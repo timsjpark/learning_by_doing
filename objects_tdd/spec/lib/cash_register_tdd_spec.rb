e32e6754feba4896d4315f89a7cb465fec787787
Tim Park <timsjpark@gmail.com>
2015-10-21 16:31:08 -0600

    Modify cash_register_tdd_spec.rb to create better contexts
    Create three contexts for pay method in order to incorporate
    suggestions made in pull request. The new contexts create
    expectations for when a payment is made that is (1) less than, (2)
    equal to, and (3) greater than the total.
    
    [#105904512]

--- objects_tdd/spec/lib/cash_register_tdd_spec.rb
+++ objects_tdd/spec/lib/cash_register_tdd_spec.rb
@@ -1,8 +1,6 @@
 require './lib/cash_register_tdd'
 
 describe CashRegister do
-  subject { CashRegister.new }
-
   describe '#total' do
     it 'should print a starting total of $0.00' do
       expect(subject.total).to eq('$0.00')
@@ -16,34 +14,68 @@ describe CashRegister do
     end
   end
 
-  describe '#purchase' do
+  describe '#purchase(amount)' do
     it 'should add a specified amount to the total' do
-      expect(subject.purchase(4.12)).to eq(4.12)
-    end
-
-    it 'should modify the total to reflect the purchase' do
       subject.purchase(4.12)
       expect(subject.total).to eq('$4.12')
     end
   end
 
-  describe '#pay' do
-    context 'should subtract a specified amount from the total and' do
-      it 'should return a string if there is payment left to be made' do
+  describe '#pay(amount)' do
+    context 'when the payment is less than the total owed' do
+      it 'should subtract a specified amount from the total' do
         subject.purchase(2.18)
-        expect(subject.pay(1.17)).to eq('You still owe $1.01')
+        subject.pay(2)
+        expect(subject.total).to eq('$0.18')
       end
-
-      it 'should return a string if the total has been fully paid' do
+      it 'should return a string that shows the remaining total' do
         subject.purchase(2.18)
-        expect(subject.pay(3)).to eq('Your change is $0.82')
+        expect(subject.pay(2)).to eq('You still owe $0.18.')
       end
     end
 
-    it 'should modify the total to reflect the payment' do
-      subject.purchase(1.23)
-      subject.pay(1.23)
-      expect(subject.total).to eq('$0.00')
+    context 'when the payment is equal to the total owed' do
+      it 'should reset the total to zero' do
+        subject.purchase(2.18)
+        subject.pay(2.18)
+        expect(subject.total).to eq('$0.00')
+      end
+      it 'should return a string that thanks you for paying the exact total' do
+        subject.purchase(2.18)
+        expect(subject.pay(2.18)).to eq('Thank you for paying the exact amount. There is no change.')
+      end
     end
+
+    context 'when the payment is greater than the total owed' do
+      it 'should reset the total to zero' do
+        subject.purchase(2.18)
+        subject.pay(3)
+        expect(subject.total).to eq('$0.00')
+      end
+      it 'should return a string that shows your change' do
+        subject.purchase(2.18)
+        expect(subject.pay(3)).to eq('Your change is $0.82.')
+      end
+    end
+
+    # context 'should subtract a specified amount from the total and' do
+    #   it 'should return a string if there is payment left to be made' do
+    #     subject.purchase(2.18)
+    #     expect(subject.pay(1.17)).to eq('You still owe $1.01')
+    #     expect(subject.total).to eq('$1.01')
+    #   end
+    #
+    #   it 'should return a string if the total has been fully paid' do
+    #     subject.purchase(2.18)
+    #     expect(subject.pay(3)).to eq('Your change is $0.82')
+    #     expect(subject.total).to eq('$0.00')
+    #   end
+    # end
+    #
+    # it 'should modify the total to reflect the payment' do
+    #   subject.purchase(1.23)
+    #   subject.pay(1.23)
+    #   expect(subject.total).to eq('$0.00')
+    # end
   end
 end
